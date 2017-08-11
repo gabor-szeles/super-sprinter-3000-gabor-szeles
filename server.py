773b05dbc162f066e53ecf70f1e1259289129c3b
gabor-szeles <szelesgbr@gmail.com>
2017-08-11 10:36:03 +0200

    refactoring done

--- server.py
+++ server.py
@@ -20,30 +20,30 @@ def route_edit():
 def route_update(story_id):
     update = 1
     database = read_database("database.csv")
-    id_ = database[int(story_id)-1][0]
-    title = database[int(story_id)-1][1]
-    story = database[int(story_id)-1][2]
-    criteria = database[int(story_id)-1][3]
-    value = database[int(story_id)-1][4]
-    estimation = database[int(story_id)-1][5]
-    status = database[int(story_id)-1][6]
-    return render_template('form.html', database=database, id=id_, title=title, story=story, criteria=criteria, value=value, estimation=estimation, status=status, update=update)
+    for data in database:
+        if data[0] == story_id:
+            id_ = data[0]
+            title = data[1]
+            story = data[2]
+            criteria = data[3]
+            value = data[4]
+            estimation = data[5]
+            status = data[6]
+    return render_template('form.html', database=database, id=id_, title=title, story=story, criteria=criteria,
+                           value=value, estimation=estimation, status=status, update=update)
 
 
 @app.route('/save-story', methods=['POST'])
 def route_save():
     database = read_database("database.csv")
-    user_story = []
+    user_story = build_story()
     try:
-        user_story.append(int(database[-1][0])+1)
+        id_list = []
+        for data in database:
+            id_list.append(int(data[0]))
+        user_story.insert(0, max(id_list)+1)
     except IndexError:
-        user_story.append("1")
-    user_story.append(request.form["title"])
-    user_story.append(request.form["story"])
-    user_story.append(request.form["criteria"])
-    user_story.append(request.form["value"])
-    user_story.append(request.form["estimation"])
-    user_story.append(request.form["status"])
+        user_story.insert(0, "1")
     database.append(user_story)
     export_data("database.csv", database)
     return redirect('/')
@@ -62,20 +62,25 @@ def route_delete(story_id):
 @app.route('/update-story/<story_id>', methods=['POST'])
 def route_save_update(story_id):
     database = read_database("database.csv")
-    user_story = []
-    user_story.append(story_id)
-    user_story.append(request.form["title"])
-    user_story.append(request.form["story"])
-    user_story.append(request.form["criteria"])
-    user_story.append(request.form["value"])
-    user_story.append(request.form["estimation"])
-    user_story.append(request.form["status"])
+    user_story = build_story()
+    user_story.insert(0, story_id)
     for lists in database:
         if lists[0] == story_id:
             database.remove(lists)
     database.append(user_story)
     export_data("database.csv", database)
     return redirect('/')
+
+
+def build_story():
+    user_story = []
+    user_story.append(request.form["title"])
+    user_story.append(request.form["story"])
+    user_story.append(request.form["criteria"])
+    user_story.append(request.form["value"])
+    user_story.append(request.form["estimation"])
+    user_story.append(request.form["status"])
+    return user_story
 
 
 def read_database(datafile):
@@ -90,9 +95,8 @@ def export_data(export_file, database):
         writer.writerows(database)
 
 
-app.secret_key = "you!never!guess!this"
-
 if __name__ == "__main__":
+    app.secret_key = "you!never!guess!this"
     app.run(
         debug=True,  # Allow verbose error reports
         port=5000  # Set custom port
